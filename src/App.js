ba24bbf0d6bc86b2f9e25385d04d634995134001
Shailesh <shaishpatel@gmail.com>
2021-01-15 12:27:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import './index.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <Link to='/'>Shopping Cart</Link>
       </header>
+      <main className="main">
+        main cart
+      </main>
+      <footer className="footer">
+        All rights is reserved
+      </footer>
     </div>
   );
 }
